a24b3808d375663102abfd9f1a72367b74acc2ed
Ironus <ironustlbb@gmail.com>
2024-09-22 10:34:36 +0200

    Armstrong numbers solution

--- armstrong-numbers/src/main/groovy/ArmstrongNumbers.groovy
+++ armstrong-numbers/src/main/groovy/ArmstrongNumbers.groovy
@@ -1,7 +1,17 @@
 class ArmstrongNumber {
+    static digitAtIndex(number, index) {
+        (number.intdiv(10.power(index))) % 10
+    }
 
     static isArmstrongNumber(number) {
-        throw new UnsupportedOperationException('method not implemented.')
+        def length = Math.ceil(Math.log10(number))
+        int calculated = 0
+
+        for (int i = 0; i < length; i++) {
+            calculated += digitAtIndex(number, i).power(length)
+        }
+
+        return number == calculated
     }
 
 }
