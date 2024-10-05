0f697f2005c926347519cd0ea2bc660af829a653
Ironus <ironustlbb@gmail.com>
2024-10-05 20:08:47 +0200

    Square-root solution - better starting point

--- square-root/src/main/groovy/SquareRoot.groovy
+++ square-root/src/main/groovy/SquareRoot.groovy
@@ -3,7 +3,7 @@ class SquareRoot {
     static Integer squareRoot(Integer radicand) {
         def f = { it -> 0.5 * (it + radicand / it) }
 
-        def root = 1
+        def root = radicand / 2
         
         while (root * root != radicand) {
             root = f.call(root)
