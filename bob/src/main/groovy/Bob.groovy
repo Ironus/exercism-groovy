f8088cccd37056bcb8fcbdaae424567013d35b35
Ironus <ironustlbb@gmail.com>
2024-10-06 17:17:04 +0200

    Bob solution - regex

--- bob/src/main/groovy/Bob.groovy
+++ bob/src/main/groovy/Bob.groovy
@@ -1,15 +1,11 @@
 class Bob {
     static response(input) {
-        input = input.grep(~/\D/).join().trim()
-
-        def question = (!input.isEmpty() && input.collect().last() == '?')
-        def shouting = (input == input.toUpperCase() && input =~ /\w+/)
-        def silence = (input.isEmpty() || input.isAllWhitespace())
-        
-        if (question && shouting) return /Calm down, I know what I'm doing!/
-        else if (question) return 'Sure.'
-        else if (shouting) return 'Whoa, chill out!'
-        else if (silence) return 'Fine. Be that way!'
-        else return 'Whatever.'
+        switch (input) {
+            case ~/[^a-z]*[A-Z][^a-z]+\?/: return "Calm down, I know what I'm doing!"
+            case ~/.+\?\s*/: return "Sure."
+            case ~/[^a-z]*[A-Z][^a-z]+!?/: return "Whoa, chill out!"
+            case ~/^\s*/: return "Fine. Be that way!"
+            default: return "Whatever."
+        }
     }
 }
