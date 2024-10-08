3a4794d0b6d8d85c131bde4f0f6ef9caa0f8c517
Ironus <ironustlbb@gmail.com>
2024-10-08 15:42:03 +0200

    Collatz-conjecture solution - iterative

--- collatz-conjecture/src/main/groovy/CollatzConjecture.groovy
+++ collatz-conjecture/src/main/groovy/CollatzConjecture.groovy
@@ -1,8 +1,14 @@
 class CollatzConjecture {
     static int steps(int number) {
         if (number < 1) throw new ArithmeticException()
-        if (number == 1) return 0
         
-        1 + steps((number%2 == 0) ? number.intdiv(2) : 3*number+1)
+        def counter = 0
+        
+        while (number != 1) {
+            counter++
+            number = (number % 2 == 0) ? number.intdiv(2) : number*3+1
+        }
+
+        counter
     }
 }
