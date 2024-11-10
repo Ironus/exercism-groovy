1cf4567cdfe8d57c6fe84371bf24d36a66e41406
Ironus <ironustlbb@gmail.com>
2024-11-10 17:30:05 +0100

    Largest-series-product - solution improved

--- largest-series-product/src/main/groovy/LargestSeriesProduct.groovy
+++ largest-series-product/src/main/groovy/LargestSeriesProduct.groovy
@@ -1,6 +1,6 @@
 class LargestSeriesProduct {
     static largestProduct(digits, span) {
-        if ((!digits && span != 0) || span > digits.size())
+        if (span > digits.size())
             throw new IllegalArgumentException("span must be smaller than string length")
         if (digits && !digits.isBigInteger())
             throw new IllegalArgumentException("digits input must only contain digits")
@@ -9,16 +9,8 @@ class LargestSeriesProduct {
         if (span == 0)
             return 1
 
-        def product = 0
-
-        digits.collect().collate(span, 1, false).collect{ it.join('') }.each{
-            def newProduct = 1
-            it.each{ newProduct *= it.toBigDecimal() }
-
-            if (newProduct > product)
-                product = newProduct
-        }
-
-        product
+        digits.collect{ it.toBigDecimal() }.collate(span, 1, false).collect{
+            it.inject(1){ acc, val -> acc * val }
+        }.max()
     }
 }
